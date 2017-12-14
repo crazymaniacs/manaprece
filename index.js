a193c7cb5e9e529938be45deb7ae06ff3b084717
Sergejs Rupainis <sergej.rupainis@gmail.com>
2017-12-14 14:50:29 +0200

    Check deployment. Attempt: 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World! New test 1');
+  res.send('Hello World! New test 2');
 });
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
