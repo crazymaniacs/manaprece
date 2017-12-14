dbe768cf6b46c37f54c4dffe56e9692e5314d6c8
mkdir <sergej.rupainis@gmail.com>
2017-12-14 21:35:59 +0200

    #13 Basic webpack configuration with babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
-const path = require("path");
+var webpack = require("webpack");
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var SRC_DIR = path.resolve(__dirname, "src");
+var DIST_DIR = path.resolve(__dirname, "dist");
 
 module.exports = {
-	entry: "./src/app.js",
+	entry: path.resolve(SRC_DIR, "app.js"),
 	output: {
-		path: path.resolve(__dirname, "dist"),
+		path: DIST_DIR,
 		filename: "bundle.js"
-	}
+	},
+	module: {
+    loaders: [
+      // JS files
+      {
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: "babel-loader",
+			},
+      // HTML files
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      }
+		]
+	},
+	plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(SRC_DIR, "index.html")
+    })
+  ],
 };
