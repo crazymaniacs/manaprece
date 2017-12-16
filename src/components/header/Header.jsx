47e946a90cebba7ddb27f61f73ccd1fbbff92115
Oskars Rukmans <oskars.rukmans@gmail.com>
2017-12-16 22:34:54 +0200

    #18: configured webpack to load images

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Ball from './ball.png'
+import './Header.scss'
 
 export class Header extends Component {
   render() {
     return (
-      <div >
-          <h1>This is a Header</h1>
+      <div className="header">
+          <img src={Ball}/>
+          <h1>This is a Header2</h1>
       </div>
     );
   }
