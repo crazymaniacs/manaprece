7081fdc4505abf18a72960e321ee388bf2d2c972
Sergejs Rupainis <sergej.rupainis@gmail.com>
2017-12-19 13:02:26 +0200

    #20 Webpack config file

---
+++ webpack.config.dev.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const SRC_DIR = path.resolve(__dirname, 'src');
+
+module.exports = {
+  entry: ['webpack-hot-middleware/client', SRC_DIR],
+  output: {
+    path: SRC_DIR,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    loaders: [
+      // JS files
+      {
+        test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react-hmre']
+        }
+      },
+      // HTML files
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      // Style files
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap']
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      // Images
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+        WEBPACK: true
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(SRC_DIR, 'index.html')
+    })
+  ]
+};
